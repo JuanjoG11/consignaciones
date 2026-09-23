c81f3ef8218c2ac97334c0507c3b965e353a17c4
JuanjoG11 <alejo32perez32@gmail.com>
2026-09-23 11:23:52 -0500

    cris men

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -88,6 +88,7 @@ export const AUXILIARES = [
   { cedula: '1004701171', nombre: 'JUAN SEBASTIAN TAMAYO PULGARIN', empresa: 'ZENU' },
   { cedula: '1088030902', nombre: 'JEAN MICHAEL ZULUAGA MORANTE', empresa: 'ZENU' },
   { cedula: '1004669887', nombre: 'CRISTIAN MAURICIO GIRALDO RAMIREZ', empresa: 'ZENU' },
+  { cedula: '1004776445', nombre: 'CRISTIAN MENDOZA', empresa: 'ZENU' },
   // TYM registrado para ambas empresas
   { cedula: '900973932',  nombre: 'TYM', empresa: 'ALPINA' },
   { cedula: '900973932',  nombre: 'TYM', empresa: 'ZENU' },
